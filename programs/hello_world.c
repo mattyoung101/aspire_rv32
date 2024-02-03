5110268bbe57881b50b51ba86710e1c2602a550b
Matt Young <matt.young.1@outlook.com>
2024-02-04 03:37:19 +1000

    Cleaned up crt.S and linker script

--- programs/hello_world.c
+++ programs/hello_world.c
@@ -24,8 +24,8 @@
 "                             ##                                 \n" \
 "                                                                \n" \
 "                     ~ THE DESIRE TO SUCCEED ~                  \n" \
-"								 \n" \
-"								 \n" \
+"								                                 \n" \
+"								                                 \n" \
 "      * (c) 2024, 2025 Matt Young. University of Queensland. *  \n" \
 
 #define TIMEOUT (5) // Timeout in seconds to re-print message
@@ -38,14 +38,15 @@ int main(void) {
     aspire_watchdog_set(false);
 
     while (1) {
-	aspire_uart_puts(ASCII_ART);
-	aspire_uart_puts("\n\nAspire is a RISC-V RV32IC_Zmmul_Zicsr CPU.\n");
-	aspire_uart_puts("This message will repeat in " xstr(TIMEOUT) " seconds.\n\n");
+        aspire_uart_puts(ASCII_ART);
+        aspire_uart_puts("\n\nAspire is a RISC-V RV32IC_Zmmul_Zicsr CPU.\n");
+        aspire_uart_puts("This message will repeat in " xstr(TIMEOUT) " seconds.\n\n");
 
-	uint64_t begin = aspire_read_cycles();
-	// block while the difference between current time and begin time is
-	// less than TIMEOUT seconds
-	while (aspire_read_cycles() - begin <= (F_CPU * TIMEOUT));
+        uint64_t begin = aspire_read_cycles();
+        // block while the difference between current time and begin time is
+        // less than TIMEOUT seconds
+        while (aspire_read_cycles() - begin <= (F_CPU * TIMEOUT));
     }
+
     return 0;
 }
