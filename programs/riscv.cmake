852475d1dfa5eb3e4da3b3ce1fdcd4ea7841d569
Matt Young <matt.young.1@outlook.com>
2024-02-03 21:25:02 +1000

    Added objcopy and corrected a few minor issues in the assembly

--- programs/riscv.cmake
+++ programs/riscv.cmake
@@ -16,22 +16,23 @@ endif()
 # https://github.com/riscv/riscv-gnu-toolchain
 find_program(RISCV_LLVM_COMPILER "$ENV{RISCV_LLVM_HOME}/bin/riscv32-unknown-elf-clang" REQUIRED)
 
-message("RISC-V LLVM found: ${RISCV_LLVM_COMPILER}")
+message(STATUS "RISC-V LLVM found: ${RISCV_LLVM_COMPILER}")
 
 get_filename_component(RISCV_TOOLCHAIN_BIN_PATH ${RISCV_LLVM_COMPILER} DIRECTORY)
 get_filename_component(RISCV_TOOLCHAIN_BIN_CLANG ${RISCV_LLVM_COMPILER} NAME_WE)
 get_filename_component(RISCV_TOOLCHAIN_BIN_EXT ${RISCV_LLVM_COMPILER} EXT)
 
-message("RISC-V LLVM toolchain path: ${RISCV_TOOLCHAIN_BIN_PATH}")
+message(STATUS "RISC-V LLVM toolchain path: ${RISCV_TOOLCHAIN_BIN_PATH}")
 
 STRING(REGEX REPLACE "\-clang" "-" CROSS_COMPILE ${RISCV_TOOLCHAIN_BIN_CLANG})
-message("RISC-V cross compile: ${CROSS_COMPILE}")
+message(STATUS "RISC-V cross compile: ${CROSS_COMPILE}")
 
 # The Generic system name is used for embedded targets (targets without OS) in
 # CMake
 set(CMAKE_SYSTEM_NAME       Generic)
 # FIXME: We want to use "rv32ic_zmmul_zicsr", but clangd has issues, see: 
 # https://lore.kernel.org/linux-patches/20230328142608.297531169@linuxfoundation.org/
+# In any case, it doesn't seem to matter as we can still use the CSR instructions regardless.
 set(CMAKE_SYSTEM_PROCESSOR  rv32ic_zmmul)
 set(ABI                     "ilp32")
 set(CMAKE_EXECUTABLE_SUFFIX ".elf")
@@ -49,13 +50,11 @@ set(CMAKE_CXX_COMPILER "${RISCV_TOOLCHAIN_BIN_PATH}/${CROSS_COMPILE}clang++")
 # We must set the OBJCOPY setting into cache so that it's available to the
 # whole project. Otherwise, this does not get set into the CACHE and therefore
 # the build doesn't know what the OBJCOPY filepath is
-set( CMAKE_OBJCOPY      ${RISCV_TOOLCHAIN_BIN_PATH}/llvm-objcopy
-     CACHE FILEPATH "The toolchain objcopy command " FORCE )
+set(CMAKE_OBJCOPY ${RISCV_TOOLCHAIN_BIN_PATH}/llvm-objcopy CACHE FILEPATH "The toolchain objcopy command" FORCE)
+set(CMAKE_OBJDUMP ${RISCV_TOOLCHAIN_BIN_PATH}/llvm-objdump CACHE FILEPATH "The toolchain objdump command" FORCE)
 
-set( CMAKE_OBJDUMP      ${RISCV_TOOLCHAIN_BIN_PATH}/llvm-objdump
-     CACHE FILEPATH "The toolchain objdump command " FORCE )
-
-# Set the common build flags
+message(STATUS "objcopy: ${CMAKE_OBJDUMP}")
+message(STATUS "objdump: ${CMAKE_OBJDUMP}")
 
 # Set the CMAKE C flags (which should also be used by the assembler!
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
